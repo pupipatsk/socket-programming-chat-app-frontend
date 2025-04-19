65d081a8af5b794243c4838845d50ef2cea127c7
pupipatsk <pupipat.sk@gmail.com>
2025-04-19 23:49:43 +0700

    fix: 24hour

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,7 +12,7 @@ export function generateId() {
 
 export function formatTime(timestamp: string) {
   const date = new Date(timestamp)
-  return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
+  return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit", hour12: false })
 }
 
 export function formatDate(timestamp: string) {
